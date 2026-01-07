9e3d09a36252e9ba55772f5727e536680cc2bf58
Agony <a03046578@gmail.com>
2026-01-07 11:07:47 +0800

    Chore: System > 專案基本設定
    1.Docker postgreSql 配置
    2.gitignore
    3.Spring boot 配置
    4.claude code 角色檔案

---
+++ src/main/java/com/example/mockodsvue/MockOdsVueApplication.java
@@ -0,0 +1,13 @@
+package com.example.mockodsvue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockOdsVueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockOdsVueApplication.class, args);
+    }
+
+}

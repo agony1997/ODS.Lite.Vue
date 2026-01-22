064f54958632fd96faaa41e4112f7c5d5d1eea71
Agony <a03046578@gmail.com>
2026-01-22 12:21:15 +0800

    Feat: Back > 調整門市商品清單 Repository

--- src/main/java/com/example/mockodsvue/repository/BranchProductListRepository.java
+++ src/main/java/com/example/mockodsvue/repository/BranchProductListRepository.java
@@ -4,7 +4,13 @@ import com.example.mockodsvue.model.entity.branch.BranchProductList;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface BranchProductListRepository extends JpaRepository<BranchProductList, Integer> {
 
+    List<BranchProductList> findByBranchCodeOrderBySortOrder(String branchCode);
+
+    Optional<BranchProductList> findByBranchCodeAndProductCodeAndUnit(String branchCode, String productCode, String unit);
 }

892636efae0fb83adcaa985d210fedeef003cf49
Agony <a03046578@gmail.com>
2026-01-22 12:21:40 +0800

    Feat: Back > 調整訂貨單 Repository

--- src/main/java/com/example/mockodsvue/repository/SalesPurchaseOrderDetailRepository.java
+++ src/main/java/com/example/mockodsvue/repository/SalesPurchaseOrderDetailRepository.java
@@ -4,7 +4,12 @@ import com.example.mockodsvue.model.entity.purchase.SalesPurchaseOrderDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SalesPurchaseOrderDetailRepository extends JpaRepository<SalesPurchaseOrderDetail, Integer> {
 
+    List<SalesPurchaseOrderDetail> findByPurchaseNoOrderByItemNo(String purchaseNo);
+
+    void deleteByPurchaseNo(String purchaseNo);
 }

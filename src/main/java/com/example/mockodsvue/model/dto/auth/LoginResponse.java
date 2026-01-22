d4216d43ed92a4465d9c8145161bf508d5973faa
Agony <a03046578@gmail.com>
2026-01-22 10:26:13 +0800

    Feat: Back > 新增認證相關 DTO

---
+++ src/main/java/com/example/mockodsvue/model/dto/auth/LoginResponse.java
@@ -0,0 +1,20 @@
+package com.example.mockodsvue.model.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponse {
+
+    private String token;
+    private String empNo;
+    private String empName;
+    private List<String> roles;
+}
